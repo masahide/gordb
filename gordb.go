135c915d9ccc6d29a1fe8ef46975c65cbe751406
YAMASAKI Masahide <masahide.y@gmail.com>
2015-04-23 02:28:13 +0900

    selection,projectionまで実装

--- gordb.go
+++ gordb.go
@@ -4,7 +4,27 @@ package gordb
 import (
 	"encoding/csv"
 	"io"
+	"strconv"
 )
+
+type Operator func(string, string) bool
+
+func atoi(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		return 0
+	}
+	return i
+}
+func greaterThan(a, b string) bool {
+	return atoi(a) > atoi(b)
+}
+func lessThan(a, b string) bool {
+	return atoi(a) < atoi(b)
+}
+func equal(a, b string) bool {
+	return atoi(a) == atoi(b)
+}
 
 type Stream interface {
 	Next() map[string]string
@@ -45,4 +65,58 @@ func (s *CSVRelationalStream) HasNext() bool {
 }
 func (s *CSVRelationalStream) Close() {
 	s.data = nil
+}
+
+type SelectionStream struct {
+	input     Stream
+	attribute string
+	selector  Operator
+	arg       string
+}
+
+func NewSelectionStream(input Stream, attribute string, selector Operator, arg string) *SelectionStream {
+	return &SelectionStream{input, attribute, selector, arg}
+}
+
+func (s *SelectionStream) Next() map[string]string {
+	tuple := s.input.Next()
+	if s.selector(tuple[s.attribute], s.arg) {
+		return tuple
+	}
+	if s.input.HasNext() {
+		return s.Next()
+	}
+	return nil
+}
+func (s *SelectionStream) HasNext() bool {
+	return s.input.HasNext()
+}
+
+func (s *SelectionStream) Close() {
+	s.input.Close()
+}
+
+type ProjectionStream struct {
+	input      Stream
+	attributes []string
+}
+
+func NewProjectionStream(input Stream, attributes []string) *ProjectionStream {
+	return &ProjectionStream{input, attributes}
+}
+
+func (s *ProjectionStream) Next() map[string]string {
+	tuple := s.input.Next()
+	result := map[string]string{}
+	for _, attribute := range s.attributes {
+		result[attribute] = tuple[attribute]
+	}
+	return result
+}
+func (s *ProjectionStream) HasNext() bool {
+	return s.input.HasNext()
+}
+
+func (s *ProjectionStream) Close() {
+	s.input.Close()
 }
--- gordb_test.go
+++ gordb_test.go
@@ -6,34 +6,67 @@ import (
 	"testing"
 )
 
-func TestNewCSVRelationalStream(t *testing.T) {
-	f, err := os.Open("1.csv")
-	if err != nil {
-		panic(err)
-		//t.Errorf("os.Open: %s", err)
-	}
-	original := NewCSVRelationalStream(f)
-	fmt.Println("<p>SELECT * FROM Staff</p>")
+func TestCSVRelationalStream(t *testing.T) {
+	staff := fopen("staff.csv")
+	defer staff.Close()
+	original := NewCSVRelationalStream(staff)
+	fmt.Println("SELECT * FROM Staff")
+	printData(original)
+	rank := fopen("rank.csv")
+	defer rank.Close()
+	original = NewCSVRelationalStream(rank)
+	fmt.Println("SELECT * FROM Rank")
 	printData(original)
 }
 
+func TestSelectionStream(t *testing.T) {
+	staff := fopen("staff.csv")
+	defer staff.Close()
+	fmt.Println("SELECT * FROM Staff WHERE age > 20")
+	relation1 := NewCSVRelationalStream(staff)
+	relation2 := NewSelectionStream(relation1, "age", greaterThan, "20")
+	printData(relation2)
+}
+
+func TestProjectionStream(t *testing.T) {
+	staff := fopen("staff.csv")
+	defer staff.Close()
+	fmt.Println("SELECT age,job FROM Staff")
+	relation1 := NewCSVRelationalStream(staff)
+	relation2 := NewProjectionStream(relation1, []string{"age", "job"})
+	printData(relation2)
+}
+
 func printData(s Stream) {
+	var cols []string
 	isHeaderWritten := false
 	for s.HasNext() {
 		row := s.Next()
 		if !isHeaderWritten {
-			fmt.Printf("|")
+			cols = make([]string, 0, len(row))
 			for col, _ := range row {
+				cols = append(cols, col)
+			}
+			fmt.Printf("|")
+			for _, col := range cols {
 				fmt.Printf("%14s|", col)
 			}
 			fmt.Printf("\n")
 			isHeaderWritten = true
 		}
 		fmt.Printf("|")
-		for col, _ := range row {
+		for _, col := range cols {
 			fmt.Printf("%14s|", row[col])
 		}
 		fmt.Printf("\n")
 	}
 	s.Close()
+}
+
+func fopen(fn string) *os.File {
+	f, err := os.Open(fn)
+	if err != nil {
+		panic(err)
+	}
+	return f
 }
