3dcab7b9b503981f20534a0d2d8d4d3dfa0d3b8c
YAMASAKI Masahide <masahide.y@gmail.com>
2015-09-27 11:51:44 +0900

    unionのテストを追加

---
+++ core/union_test.go
@@ -0,0 +1,50 @@
+package core
+
+import (
+	"encoding/json"
+	"log"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+/*
+	"selection": {
+		"input": { "union":{
+				"input1":{"relation": {"name": "test/staff1"}},
+				"input2":{"relation": {"name": "test/rank1"}}
+		}},
+		"attr": "name", "selector": ">", "arg": 0
+	}
+*/
+
+func TestJsonUnionDecode(t *testing.T) {
+	var want = &Relation{
+		Attrs: Schema{Attr{"name", reflect.String}, Attr{"age", reflect.Int64}, Attr{"job", reflect.String}},
+		Data: [][]Value{
+			[]Value{"田中", int64(34), "デザイナー"},
+			[]Value{"田中", int64(34), "デザイナー"},
+		},
+	}
+
+	const jsonStream = `{
+		"selection": {
+			"input": { 
+				"union":{
+					"input1":{"relation": {"name": "test/staff1"}},
+					"input2":{"relation": {"name": "test/staff1"}}
+				}
+			},	
+			"attr": "name", "selector": "==", "arg": "田中"
+		}
+	}`
+	m := Stream{}
+	if err := json.NewDecoder(strings.NewReader(jsonStream)).Decode(&m); err != nil {
+		log.Fatal(err)
+	}
+	result, _ := StreamToRelation(m, testData)
+	if !reflect.DeepEqual(result.Data, want.Data) {
+		t.Errorf("Does not match result:%# v, want:%# v", result.Data, want.Data)
+	}
+
+}
